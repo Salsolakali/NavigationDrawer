1a996f6f11e380850e446c5139c29f2e487e9b5a
EU\FernanA6 <alberto.fernandezseral@unisys.com>
2018-04-20 11:18:44 +0200

    Final

--- app/src/main/java/com/example/fernana6/navigationdrawer/NavigationDrawerActivity.java
+++ app/src/main/java/com/example/fernana6/navigationdrawer/NavigationDrawerActivity.java
@@ -1,8 +1,11 @@
 package com.example.fernana6.navigationdrawer;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,7 +17,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class NavigationDrawerActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener ,
+        Menu1Fragment.OnFragmentInteractionListener,
+        Menu2Fragment.OnFragmentInteractionListener,
+        Menu3Fragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +46,9 @@ public class NavigationDrawerActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //add this line to display menu1 when the activity is loaded
+        ///displaySelectedScreen(R.id.menu1);
     }
 
     @Override
@@ -77,25 +86,40 @@ public class NavigationDrawerActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        displaySelectedScreen(item.getItemId());
+        return true;
+    }
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+    private void displaySelectedScreen (int itemId){
+        //creating fragment object
+        Fragment fragment = null;
 
-        } else if (id == R.id.nav_slideshow) {
+        //initializing the fragment object which is selected
+        switch (itemId) {
+            case R.id.menu1:
+                fragment = new Menu1Fragment();
+                break;
+            case R.id.menu2:
+                fragment = new Menu2Fragment();
+                break;
+            case R.id.menu3:
+                fragment = new Menu3Fragment();
+                break;
+        }
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        //replacing the fragment
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
